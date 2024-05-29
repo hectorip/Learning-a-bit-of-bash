9acdb7c3a5acfd2a28c4bbfb5c09778d6fe3f3d5
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-05-28 22:29:28 -0600

    Redirects y globs

---
+++ globs.sh
@@ -0,0 +1,17 @@
+# los globs expanden a los archivos que coincidan con el patrón
+# que no es exactamente igual que una expresión regular
+#
+# * - expande a 0 o más caracteres, excepto / y . cuando está al principio de los archivos
+# ? - expande exactamente a un carácter
+# [abc] - expande a un solo carácter que sea a, b o c
+
+ls glob_test/*.sh # a.sh b.sh c.sh
+echo "*.txt"
+ls glob_test/*.txt # *.txt
+echo "*.*"
+ls glob_test/*.*
+echo "-----"
+echo ".*"
+echo "-----"
+
+ls glob_test/.*
---
+++ redirects.sh
@@ -0,0 +1,5 @@
+# Todos los programas tienen una salida
+# estándar y una salida de error
+# Puedes redirgir ambas
+
+ls . >files.txt 2>error.txt
